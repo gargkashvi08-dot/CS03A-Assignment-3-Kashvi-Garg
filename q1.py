e6954819f5fe8dabb35566cfa15daff9b1ac4beb
gargkashvi08-dot <gargkashvi08@gmail.com>
2026-08-01 22:00:55 -0700

    q1.py

--- q1.py
+++ q1.py
@@ -8,27 +8,27 @@
 import math
 
 class Circle:
-    def __init__(self,x=0,y=0,radius=1):
-        self.x=x
-        self.y=y
+    def __init__(self, x=0,y=0,radius=1):
+        self.x= x
+        self.y =y
         self.radius=radius
 
     def getX(self):
         return self.x
 
     def setX(self,x):
-        self.x=x
+        self.x = x
 
     def getY(self):
         return self.y
 
-    def setY(self,y):
+    def setY(self, y):
         self.y=y
 
     def getRadius(self):
         return self.radius
 
-    def setRadius(self,radius):
+    def setRadius(self, radius):
         self.radius=radius
 
     def getArea(self):
@@ -38,25 +38,25 @@ class Circle:
         return 2*math.pi*self.radius
 
     def containPoint(self,x,y):
-        dist=((x-self.x)**2+(y-self.y)**2)**0.5
+        dist=((x -self.x)**2+(y- self.y)**2)**0.5
         return dist<=self.radius
 
     def containCircle(self,circle):
-        dist=((circle.x-self.x)**2+(circle.y-self.y)**2)**0.5
-        return dist<=self.radius-circle.radius
+        dist=((circle.x- self.x)**2+(circle.y-self.y)**2)**0.5
+        return dist<=self.radius - circle.radius
 
     def overlaps(self,circle):
-        dist=((circle.x-self.x)**2+(circle.y-self.y)**2)**0.5
+        dist=((circle.x- self.x)**2+(circle.y -self.y)**2)**0.5
         return dist<=self.radius+circle.radius
 
 def run():
-    circles=[Circle(0,0,5),Circle(1,1,2),Circle(10,10,3),Circle(0,0,1)]
+    circles= [Circle(0,0,5), Circle(1,1,2), Circle(10,10,3),Circle(0,0,1)]
 
     for c in circles:
-        print('Area: '+str(c.getArea())+' Perimeter: '+str(c.getPerimeter()))
+        print('Area: '+ str(c.getArea())+' Perimeter: '+ str(c.getPerimeter()))
 
     for c in circles:
-        print('Contains (5,5): '+str(c.containPoint(5,5)))
+        print('Contains (5,5): '+   str(c.containPoint(5,5)))
 
     for a in circles:
         for b in circles:
@@ -66,7 +66,7 @@ def run():
     for a in circles:
         for b in circles:
             if a is not b:
-                print('Overlaps other circle: '+str(a.overlaps(b)))
+                print('Overlaps other circle: '+ str(a.overlaps(b)))
 
 if __name__=='__main__':
     run()
