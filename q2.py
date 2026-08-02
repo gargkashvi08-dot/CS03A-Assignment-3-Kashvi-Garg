669af09198c904214b21a5021a012da4cdc32499
gargkashvi08-dot <gargkashvi08@gmail.com>
2026-08-01 22:12:52 -0700

    q2.py

--- q2.py
+++ q2.py
@@ -6,57 +6,57 @@
 # /*****************************************************/
 
 class Employee:
-    def __init__(self,name='',number=0):
-        self.name=name
-        self.number=number
+    def __init__(self,name='', number=0):
+        self.name= name
+        self.number =number
 
-    def getName(self):
+    def getName(self ):
         return self.name
 
-    def setName(self,name):
+    def setName(self , name):
         self.name=name
 
-    def getNumber(self):
+    def getNumber(self ):
         return self.number
 
-    def setNumber(self,number):
+    def setNumber(self, number):
         self.number=number
 
 class ProductionWorker(Employee):
-    def __init__(self,name='',number=0,shift=1,payRate=0.0):
+    def __init__(self, name='',number= 0,shift=1, payRate=0.0):
         super().__init__(name,number)
-        self.shift=shift
+        self.shift= shift
         self.payRate=payRate
 
     def getShift(self):
         return self.shift
 
     def setShift(self,shift):
-        self.shift=shift
+        self.shift =shift
 
     def getPayRate(self):
         return self.payRate
 
     def setPayRate(self,payRate):
-        self.payRate=payRate
+        self.payRate = payRate
 
 def run():
-    worker=ProductionWorker()
+    worker =ProductionWorker()
 
-    name=input('Enter employee name: ')
-    number=input('Enter employee number: ')
-    shift=int(input('Enter shift (1 for day, 2 for night): '))
-    payRate=float(input('Enter hourly pay rate: '))
+    name =input('Enter employee name: ')
+    number =input('Entar employee number: ')
+    shift =int(input('Enter shift (1 for day, 2 for night): '))
+    payRate =float(input('enter hourly pay rate: '))
 
     worker.setName(name)
     worker.setNumber(number)
     worker.setShift(shift)
     worker.setPayRate(payRate)
 
-    print('Name: '+worker.getName())
-    print('Number: '+worker.getNumber())
-    print('Shift: '+str(worker.getShift()))
-    print('Pay rate: '+str(worker.getPayRate()))
+    print('Name: ' + worker.getName())
+    print('Number: ' + worker.getNumber())
+    print('Shift: ' +str(worker.getShift()))
+    print('Pay rate: ' +str(worker.getPayRate()))
 
 if __name__=='__main__':
     run()
